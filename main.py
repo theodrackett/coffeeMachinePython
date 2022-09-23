b0f457067c7fd5de4b74b998c86b5f0a3400a5aa
Theo Drackett <yawdie@hotmail.com>
2022-09-23 06:40:01 -0700

    Menu completed

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is my new coffee maker. It makes coffee for all my favorite people.
+# Actually it will make coffee for my not so favorite people also, but they have to pay more
+
+# These are the items that my coffee maker will make for you
+Menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18
+        },
+        "cost": 1.5
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "cost": 2.5
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24
+        },
+        "cost": 3.0
+    }
+}
